bebd8b617c11fcba58084e3291bee783147998a0
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-02-25 02:00:02 +0600

    feat: add code of functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
+/* jshint esversion: 6 */
 exports.min = function min (array) {
-  return 0;
-}
+  if (Array.isArray(array) && !array.length || !array) {
+    return 0;
+  }
+  let result;
+  let compareNum = array[0];
+  for (const item of array) {
+    if (item <= compareNum) {
+      compareNum = item;
+      result = item;
+    }
+  }
+  return result;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (Array.isArray(array) && !array.length || !array) {
+    return 0;
+  }
+  let result;
+  let compareNum = array[0];
+  for (const item of array) {
+    if (item >= compareNum) {
+      compareNum = item;
+      result = item;
+    }
+  }
+  return result;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (Array.isArray(array) && !array.length || !array) {
+    return 0;
+  }
+  let sum = 0;
+  for (const item of array) {
+    sum += item;
+  }
+  return sum / array.length;
+};
